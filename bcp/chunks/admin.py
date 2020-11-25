22031f357ba88f25e3f6f1927fd17a84b880aac6
Adam Olsen <arolsen@gmail.com>
2020-11-25 10:05:50 -0700

    update style

--- bcp/chunks/admin.py
+++ bcp/chunks/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
+
 from bcp.chunks.models import Chunk
 
+
 class ChunkAdmin(admin.ModelAdmin):
-  list_display = ('key',)
-  search_fields = ('key', 'content')
+    list_display = ('key',)
+    search_fields = ('key', 'content')
+
 
 admin.site.register(Chunk, ChunkAdmin)
--- bcp/wsgi.py
+++ bcp/wsgi.py
@@ -3,6 +3,6 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bcp.settings")
 
 # This application object is used by the development server
 # as well as any WSGI server configured to use this file.
-from django.core.wsgi import get_wsgi_application
+from django.core.wsgi import get_wsgi_application  # noqa
 
 application = get_wsgi_application()
